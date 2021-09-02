a188e4ca40cd53044a9c0a7d83c00cc0b03674e5
thorbjorga <81170965+thorbjorga@users.noreply.github.com>
2021-09-02 10:49:45 +0000

    Update max_int.py

--- max_int.py
+++ max_int.py
@@ -5,8 +5,11 @@
 # Then implement the algorithm in Python. Put your algorithmic description as a comment in the program file.
 
 #1. Start by getting an input from the user.
-#2. Create a constant that holds the highes number, starting at 0.
+#2. Create a constant that holds the highest number, starting at 0.
 #3. Use a while loop to repeatly ask the user for a number until he enters a negative value.
+#4. Max_int will store the largest number inputted
+#5. Everytime a user inputs a number greater then the previous max_int, a new highest number will be set to the max_int
+#6. When a negative value is entered, the program will print out the largest number that the user has entered.
 
 
 
